5fade12ac42f1816d527984289e2830e326cf31b
steizy <steizym.fornica2@itjo.edu.ve>
2025-05-01 16:56:37 -0400

    Estructura principal - main c++

---
+++ Index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Proyecto 1"<<endl;
+    cout<<"Autoras: Amelie Moreno"<<endl;
+    cout<<"         Gabriela Cantos"<<endl;
+    cout<<"         Steizy Fornica"<<endl;
+    
+    return 0;
+}
